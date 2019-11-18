f9b6aff3a6f33db8d8a2b78a3b1d8e54a372dbdb
Vincent Andrieu <vincent.andrieu@epietch.eu>
2019-11-18 16:15:10 +0100

    - Add criterion tests

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@
 ##
 
 SRC =   src/main.c	\
+		src/bsq.c	\
 		src/square.c
 
 OBJ =   $(SRC:.c=.o)
@@ -15,11 +16,16 @@ NAME    =   bsq
 CFLAGS  +=  -I include -Wall -Wextra
 LDFLAGS +=  -L lib/my -lmy
 
+TEST	=	@gcc -o unit_tests src/bsq.c src/square.c tests/tests_bsq.c $(LDFLAGS) $(CFLAGS) --coverage -lcriterion
+
 all:    $(NAME)
 
 $(NAME):    $(OBJ)
 	make -C lib/my/
 	gcc -o $(NAME) $(OBJ) $(LDFLAGS)
+
+tests_run:
+	$(TEST)
 
 clean:
 	@make clean -C lib/my/ --no-print-directory
---
+++ tests/tests_bsq.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2019
+** BSQ
+** File description:
+** tests criterion
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "bsq.h"
+
+Test(bsq_1, test_outputs) {
+    cr_redirect_stdout();
+    
+    char *filepath = "maps/tests_maps_solved/intermediate_map_34_137_with_obstacles_25pc";
+    struct stat st;
+    stat(filepath, &st);
+    int fd = open(filepath, O_RDONLY);
+    char *buffer = malloc(sizeof(char) * st.st_size);
+    read(fd, buffer, st.st_size);
+    
+    close(fd);
+    bsq_main("maps/tests_maps/intermediate_map_34_137_with_obstacles_25pc");
+    cr_assert_stdout_eq_str(buffer);
+    free(buffer);
+}
+
+Test(bsq, test_outputs_no_number_file) {
+    cr_redirect_stdout();
+    
+    bsq_main("maps/tests_maps/map_no_number");
+    cr_assert_stdout_eq_str("First line isn't a number\n");
+}
+
+Test(bsq, test_outputs_empty_file) {
+    cr_redirect_stdout();
+    
+    bsq_main("maps/tests_maps/map_empty");
+    cr_assert_stdout_eq_str("Empty file\n");
+}
+
+Test(bsq, test_outputs_read_failure) {
+    cr_redirect_stdout();
+    
+    bsq_main("");
+    cr_assert_stdout_eq_str("Read failed\n");
+}
