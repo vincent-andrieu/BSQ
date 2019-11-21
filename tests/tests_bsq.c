083137de597ae2e60412143999c07bcc4e84f09a
Vincent Andrieu <vincent.andrieu@epietch.eu>
2019-11-21 09:51:33 +0100

    - fix style (bad indentation)

--- tests/tests_bsq.c
+++ tests/tests_bsq.c
@@ -15,7 +15,7 @@
 Test(bsq_1, test_outputs)
 {
     cr_redirect_stdout();
-    
+
     char *filepath = "maps/tests_maps_solved/intermediate_"
     "map_34_137_with_obstacles_25pc";
     struct stat st;
@@ -23,7 +23,7 @@ Test(bsq_1, test_outputs)
     int fd = open(filepath, O_RDONLY);
     char *buffer = malloc(sizeof(char) * st.st_size);
     read(fd, buffer, st.st_size);
-    
+
     close(fd);
     bsq_main("maps/tests_maps/intermediate_map_34_137_with_obstacles_25pc");
     cr_assert_stdout_eq_str(buffer);
@@ -33,7 +33,7 @@ Test(bsq_1, test_outputs)
 Test(bsq, test_outputs_no_number_file)
 {
     cr_redirect_stderr();
-    
+
     bsq_main("maps/tests_maps/map_no_number");
     cr_assert_stderr_eq_str("First line isn't a number\n");
 }
@@ -41,7 +41,7 @@ Test(bsq, test_outputs_no_number_file)
 Test(bsq, test_outputs_empty_file)
 {
     cr_redirect_stderr();
-    
+
     bsq_main("maps/tests_maps/map_empty");
     cr_assert_stderr_eq_str("Empty file\n");
 }
@@ -49,7 +49,7 @@ Test(bsq, test_outputs_empty_file)
 Test(bsq, test_outputs_read_failure)
 {
     cr_redirect_stderr();
-    
+
     bsq_main("");
     cr_assert_stderr_eq_str("Read failed\n");
 }
@@ -57,7 +57,7 @@ Test(bsq, test_outputs_read_failure)
 Test(bsq, test_outputs_invalid_number_lines)
 {
     cr_redirect_stderr();
-    
+
     bsq_main("maps/tests_maps/map_invalid_nbr_lines");
     cr_assert_stderr_eq_str("Invalid number of lines\n");
 }
@@ -65,7 +65,7 @@ Test(bsq, test_outputs_invalid_number_lines)
 Test(bsq, test_outputs_invalid_line_size)
 {
     cr_redirect_stderr();
-    
+
     bsq_main("maps/tests_maps/map_invalid_line_size");
     cr_assert_stderr_eq_str("ERROR\n");
 }
